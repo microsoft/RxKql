bbea1e0394c5383e7453bd011326020ac24ae6f5
georgis <georgis@microsoft.com>
2021-04-23 06:43:41 -0700

    Simplify the code in the MouseMove sample

--- Samples/Rx.Kql.MouseMove/Form1.cs
+++ Samples/Rx.Kql.MouseMove/Form1.cs
@@ -1,107 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Net.Mime;
 using System.Reactive.Kql;
 using System.Reactive.Linq;
-using System.Reflection;
-using System.Runtime.Hosting;
 using System.Windows.Forms;
 
 namespace RxWinforms
 {
-    // This sample illustrates the original usage of Reactive Extensions for event-driven UI
-    // Good book with more examples is "Programming Reactive Extensions and LINQ" http://www.apress.com/9781430237471
+    // This sample illustrates how Rx.KQL translates Kusto Queries to Reactive Extensions
+    // For reference see the original sample: (http://github.com/Microsoft/Tx/blob/master/Samples/RxWinforms/Readme.md
+    //
+    // Here the LINQ verb KustoQuery(...) allows the user to filter and transform events in KQL
+    // ... as oposed to learning lambda exppressions, Disposables, C# types, etc.
     public partial class Form1 : Form
     {
-        private int leftButtonCounter = 0;
-        private int rightButtonCounter = 0;
-        private int noButtonCounter = 0;
-
-        List<IDisposable> _subscriptions = new List<IDisposable>();
-
+        IDisposable _subscription;
+        private int eventCounter = 0;
         public Form1()
         {
             InitializeComponent();
 
-            var assembly = Assembly.GetExecutingAssembly().GetName();
-            this.Text = $@"{assembly.Name} [{assembly.Version}]";
+            var pen = new Pen(Color.Gold, 2);
 
-            var leftButtonPen = new Pen(Color.LightGreen, 2);
-            leftButtonTextBox.ForeColor = Color.LightGreen;
-
-            var rightButtonPen = new Pen(Color.Gold, 2);
-            rightButtonTextBox.ForeColor = Color.Gold;
-
-            var noButtonPen = new Pen(Color.Gainsboro, 2);
-            noButtonTextBox.ForeColor = Color.Gainsboro;
-
-            var leftButtonEvents = Observable.FromEventPattern<MouseEventArgs>(panel1, "MouseMove")
+            var points = Observable.FromEventPattern<MouseEventArgs>(panel1, "MouseMove")
                 .ToDynamic(m => m.EventArgs)
                 .KustoQuery("where Button == \"Left\" | project X, Y");
 
-            // Register Left button events
-            _subscriptions.Add(leftButtonEvents.Subscribe(p =>
+            _subscription = points.Subscribe(p =>
             {
-                panel1.CreateGraphics().FillRectangle(leftButtonPen.Brush, float.Parse(p["X"].ToString()), float.Parse(p["Y"].ToString()), 5, 5);
-
-                SetEventCounterDisplay(leftButtonTextBox, leftButtonCounter++);
-                SetEventCounterDisplay(noButtonTextBox, noButtonCounter++);
-            }));
-
-            // Register Right button events
-            var rightButtonEvents = Observable.FromEventPattern<MouseEventArgs>(panel1, "MouseMove")
-                .ToDynamic(m => m.EventArgs)
-                .KustoQuery("where Button == \"Right\" | project X, Y");
-
-            _subscriptions.Add(rightButtonEvents.Subscribe(p =>
-            {
-                panel1.CreateGraphics().FillRectangle(rightButtonPen.Brush, float.Parse(p["X"].ToString()), float.Parse(p["Y"].ToString()), 5, 5);
-                ;
-                SetEventCounterDisplay(rightButtonTextBox, rightButtonCounter++);
-                SetEventCounterDisplay(noButtonTextBox, noButtonCounter++);
-            }));
-
-            // Register Right button events
-            var noButtonEvents = Observable.FromEventPattern<MouseEventArgs>(panel1, "MouseMove")
-                .ToDynamic(m => m.EventArgs)
-                .KustoQuery("where Button != \"Right\" and Button != \"Left\" | project X, Y");
-
-            _subscriptions.Add(noButtonEvents.Subscribe(p =>
-            {
-                if (toggleAllEventsToolStripMenuItem.Checked)
-                    panel1.CreateGraphics().FillRectangle(noButtonPen.Brush, float.Parse(p["X"].ToString()), float.Parse(p["Y"].ToString()), 5, 5);
-
-                SetEventCounterDisplay(noButtonTextBox, noButtonCounter++);
-            }));
-        }
-
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void clearAllDataPointsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ResetCounters();
-            panel1.Invalidate();
-        }
-
-        private void resetCountersToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ResetCounters();
-        }
-
-        private void ResetCounters()
-        {
-            SetEventCounterDisplay(leftButtonTextBox, leftButtonCounter = 0);
-            SetEventCounterDisplay(noButtonTextBox, rightButtonCounter = 0);
-            SetEventCounterDisplay(rightButtonTextBox, noButtonCounter = 0);
-        }
-
-        private void SetEventCounterDisplay(TextBox textBox, int value)
-        {
-            textBox.Text = $@"Moves: {value}";
+                int X = Convert.ToInt32(p["X"]);
+                int Y = Convert.ToInt32(p["Y"]);
+                panel1.CreateGraphics().FillRectangle(pen.Brush, X, Y, 5, 5);
+                textBox1.Text = $"{eventCounter++}";
+            });
         }
     }
 }
